471f7a6c7793fc1e99119220d8058e83796b1b19
lchanss <lchans0319@gmail.com>
2025-10-17 22:35:53 +0900

    feat: 헤더 날짜 선택 섹션 UI 구현

--- src/components/header/MonthNavigator.tsx
+++ src/components/header/MonthNavigator.tsx
@@ -1,13 +1,20 @@
+import LeftIcon from "@/assets/icons/chevron-left.svg?react";
+import RightIcon from "@/assets/icons/chevron-right.svg?react";
+
 export default function MonthNavigator() {
   return (
-    <div className="">
-      <button aria-label="이전 달">‹</button>
-      <time>
+    <div className="flex items-center justify-center gap-6">
+      <button aria-label="이전 달" className="cursor-pointer">
+        <LeftIcon width={32} height={32} />
+      </button>
+      <time className="flex w-30 flex-col items-center justify-center gap-1">
         <span className="text-light-14">2023</span>
         <span className="text-serif-48">8</span>
         <span className="text-light-14">August</span>
       </time>
-      <button aria-label="다음 달">›</button>
+      <button aria-label="다음 달" className="cursor-pointer">
+        <RightIcon width={32} height={32} />
+      </button>
     </div>
   );
 }
