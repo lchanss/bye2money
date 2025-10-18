80e1b3eea91295e66daaef3130ed11c15c462ab8
lchanss <lchans0319@gmail.com>
2025-10-18 19:19:31 +0900

    feat: 날짜 이동 버튼 로직 구현

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,33 @@
+import { useState } from "react";
+
 import AppTitle from "./AppTitle";
 import MonthNavigator from "./MonthNavigator";
 import ViewSelector from "./ViewSelector";
 
+const today = new Date();
+
+// TODO: 전역 상태로 날짜 관리
 export default function Header() {
+  const [date, setDate] = useState(today);
+
+  const handlePrevClick = () => {
+    const prevMonth = new Date(date.getFullYear(), date.getMonth() - 1, 1);
+    setDate(prevMonth);
+  };
+
+  const handleNextClick = () => {
+    const nextMonth = new Date(date.getFullYear(), date.getMonth() + 1, 1);
+    setDate(nextMonth);
+  };
+
   return (
     <header className="bg-colorchip-80 flex h-54 items-center justify-center gap-44 pt-10 pb-16">
       <AppTitle />
-      <MonthNavigator />
+      <MonthNavigator
+        date={date}
+        onPrevClick={handlePrevClick}
+        onNextClick={handleNextClick}
+      />
       <ViewSelector />
     </header>
   );
--- src/components/header/MonthNavigator.tsx
+++ src/components/header/MonthNavigator.tsx
@@ -1,18 +1,30 @@
 import LeftIcon from "@/assets/icons/chevron-left.svg?react";
 import RightIcon from "@/assets/icons/chevron-right.svg?react";
 
-export default function MonthNavigator() {
+type MonthNavigatorProps = {
+  date: Date;
+  onPrevClick: () => void;
+  onNextClick: () => void;
+};
+
+export default function MonthNavigator({
+  date,
+  onPrevClick,
+  onNextClick,
+}: MonthNavigatorProps) {
   return (
     <div className="flex items-center justify-center gap-6">
-      <button aria-label="이전 달" className="cursor-pointer">
+      <button aria-label="이전 달" onClick={onPrevClick}>
         <LeftIcon width={32} height={32} />
       </button>
       <time className="flex w-30 flex-col items-center justify-center gap-1">
-        <span className="text-light-14">2023</span>
-        <span className="text-serif-48">8</span>
-        <span className="text-light-14">August</span>
+        <span className="text-light-14">{date.getFullYear()}</span>
+        <span className="text-serif-48">{date.getMonth() + 1}</span>
+        <span className="text-light-14">
+          {date.toLocaleString("en-US", { month: "long" })}
+        </span>
       </time>
-      <button aria-label="다음 달" className="cursor-pointer">
+      <button aria-label="다음 달" onClick={onNextClick}>
         <RightIcon width={32} height={32} />
       </button>
     </div>
