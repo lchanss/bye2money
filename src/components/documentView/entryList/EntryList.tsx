7b73dda67082b61f94922d8ec0ef4e09051ce68f
lchanss <lchans0319@gmail.com>
2025-10-28 19:11:57 +0900

    feat: 수입/지출 내역 필터링 구현

--- src/components/documentView/entryList/EntryList.tsx
+++ src/components/documentView/entryList/EntryList.tsx
@@ -1,15 +1,59 @@
+import { useState } from "react";
+
 import type { GetEntryListResponse } from "@/apis/entry";
-import type { Entry } from "@/types";
+import Checkbox from "@/components/common/Checkbox";
+import type { EntryType } from "@/types";
 
 type EntryListProps = {
   entryList: GetEntryListResponse;
 };
 
 export default function EntryList({ entryList }: EntryListProps) {
+  const [filter, setFilter] = useState<Record<EntryType, boolean>>({
+    income: true,
+    expense: true,
+  });
+
+  const toggleFilter = (type: EntryType) => {
+    setFilter((prev) => ({ ...prev, [type]: !prev[type] }));
+  };
+
   return (
     <div>
-      EntryList
+      <EntrySummary
+        summary={entryList.summary}
+        filter={filter}
+        onChangeFilter={toggleFilter}
+      />
       <div></div>
+    </div>
+  );
+}
+
+type EntrySummaryProps = {
+  summary: GetEntryListResponse["summary"];
+  filter: Record<EntryType, boolean>;
+  onChangeFilter: (type: EntryType) => void;
+};
+
+function EntrySummary({ summary, filter, onChangeFilter }: EntrySummaryProps) {
+  const { totalCount, totalIncome, totalExpense } = summary;
+
+  return (
+    <div className="mx-6 mb-10 flex items-center justify-between">
+      <span>전체 내역 {totalCount}건</span>
+      <div className="flex items-center gap-3">
+        <Checkbox
+          checked={filter.income}
+          label={`수입 ${totalIncome.toLocaleString()}`}
+          onChange={() => onChangeFilter("income")}
+        />
+        <Checkbox
+          checked={filter.expense}
+          label={`지출 ${totalExpense.toLocaleString()}`}
+          onChange={() => onChangeFilter("expense")}
+        />
+      </div>
     </div>
   );
 }
